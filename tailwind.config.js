f48a9ecfd6c677358b7f36c9717aee4a2e4eef55
mohammadali <mohammad8383ali@gmail.com>
2022-08-20 17:37:19 +0430

    The first phase of the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      width: {
+        '13': '3.25rem',
+        '90px': '90px',
+        '15': '3.75rem',
+        '70px': '70px'
+      }
+    },
+  },
+  plugins: [],
+}
