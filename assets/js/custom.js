405c25affb52fecba198cc12703ae8b8237859f5
mohammadali <mohammad8383ali@gmail.com>
2022-08-23 02:17:39 +0430

    The fourth phase of the project

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,4 +3,13 @@ var swiper = new Swiper(".top-slider", {
     pagination: {
         el: ".top-slider-pagination",
     },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    }
 });
+
+function setLocation(e) {
+    e.target.classList.remove('flex')
+    e.target.classList.add('hidden')
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,15 @@ module.exports = {
       },
       backgroundImage: {
         'digiplus': "url('/assets/img/plus-widget.png')",
-        'digiclub': "linear-gradient(76.02deg,#35a6c8 33.32%,#007295 71.55%),radial-gradient(21.29% 87.33% at 50% 50.67%,#7cf2f6 0,#35a6c8 100%)"
+        'digiclub': "linear-gradient(76.02deg, #35a6c8 33.32%, #007295 71.55%), radial-gradient(21.29% 87.33% at 50% 50.67%, #7cf2f6 0 ,#35a6c8 100%)",
+        'amazing': "linear-gradient(to top, #ef4056, #ef394e)",
+        'fresh': "url('/assets/img/freshPattern.svg')"
       },
       backgroundSize: {
         'w100': '100% auto'
+      },
+      backgroundPosition: {
+        'left-center': "left center"
       }
     },
   },
