cee2910a9cad301231aa45a6a16dc2d89a0de0e9
mohammadali <mohammad8383ali@gmail.com>
2022-08-27 06:25:22 +0430

    The fifth phase of the project

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,7 +9,24 @@ var swiper = new Swiper(".top-slider", {
     }
 });
 
-function setLocation(e) {
+function backToTop() {
+    window.scrollTo({
+        top: '0',
+        left: '0',
+        behavior: 'smooth'
+    });
+}
+
+// remove
+
+function hidd(e) {
     e.target.classList.remove('flex')
     e.target.classList.add('hidden')
 }
+
+
+// window.onload = () => window.scrollTo({
+//     top: document.documentElement.scrollHeight,
+//     left: '0',
+//     behavior: 'smooth'
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './assets/js/custom.js'],
   theme: {
     extend: {
       width: {
